bb89b2121bbc9b1d4d82dc926b2297678480ad07
Radoslaw Zarzynski <rzarzynski@github.com>
2015-10-05 16:11:43 +0200

    env_setup_swift_tempauth.sh: fix reversion of params in X-Auth-User.
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@github.com>

--- env_setup_swift_tempauth.sh
+++ env_setup_swift_tempauth.sh
@@ -4,7 +4,7 @@
 
 source $(dirname ${BASH_SOURCE[0]})/config.sh
 
-response=$( curl -i -H  X-Auth-User:${TEMPAUTH_USER}:${TEMPAUTH_ACCT}   \
+response=$( curl -i -H  X-Auth-User:${TEMPAUTH_ACCT}:${TEMPAUTH_USER}   \
                     -H  X-Auth-Key:${TEMPAUTH_PASS}                     \
                     ${TEMPAUTH_URL}                                     \
             | tr -d '\r')
