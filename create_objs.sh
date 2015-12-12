9ae11b05fc7d875fe96f85efd1833748896842b8
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2015-12-12 19:33:27 +0100

    create_objs.sh: add SLO/DLO creation.
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- create_objs.sh
+++ create_objs.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
-curl -i ${publicURL}/cont -X PUT -H "X-Auth-Token: ${token}"
+curl -i ${publicURL}/cont       -X PUT -H "X-Auth-Token: ${token}"
 
-#curl -i http://localhost:8080/swift/v1/cont/aaa10 -X PUT -H "X-Auth-Token: $token" -H "Content-Length: 0"
-#curl -i http://localhost:8080/swift/v1/cont/obj10 -X PUT -H "X-Auth-Token: $token" -H "Content-Length: 0"
-#curl -i http://localhost:8080/swift/v1/cont/obj15 -X PUT -H "X-Auth-Token: $token" -H "Content-Length: 0"
-#curl -i http://localhost:8080/swift/v1/cont/obj12 -X PUT -H "X-Auth-Token: $token" -H "Content-Length: 0"
+curl -i ${publicURL}/cont/aaa10 -X PUT -H "X-Auth-Token: ${token}" \
+     -d '123'
 
-curl -i ${publicURL}/cont/aaa10 -X PUT -H "X-Auth-Token: ${token}" -d ''
-curl -i ${publicURL}/cont/obj10 -X PUT -H "X-Auth-Token: ${token}" -d ''
-curl -i ${publicURL}/cont/obj15 -X PUT -H "X-Auth-Token: ${token}" -d ''
-curl -v ${publicURL}/cont/obj12 -X PUT -H "X-Auth-Token: ${token}" -d ''
+# DLO/SLO's segments
+curl -i ${publicURL}/cont/obj10 -X PUT -H "X-Auth-Token: ${token}" \
+     -d 'abc'
+curl -i ${publicURL}/cont/obj15 -X PUT -H "X-Auth-Token: ${token}" \
+     -d 'ABC'
+curl -i ${publicURL}/cont/obj12 -X PUT -H "X-Auth-Token: ${token}" \
+     -d 'xyz'
+
+# DLO
+curl -v ${publicURL}/cont/dlo   -X PUT -H "X-Auth-Token: ${token}" \
+     -d 'ok'                                                       \
+     -H "X-Object-Manifest: cont/obj"
+
+# SLO
+curl -v "${publicURL}/cont/slo?multipart-manifest=put"             \
+                                -X PUT -H "X-Auth-Token: ${token}" \
+     -d "`cat statics/slo_manifest.txt`"
