e579022e09967039529e2c1ae5079e9a9847fe4b
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2015-12-19 18:04:44 +0100

    create_s3-tests_users.sh: initial version.
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

---
+++ create_s3-tests_users.sh
@@ -0,0 +1,13 @@
+./radosgw-admin user create                             \
+        --uid="s3_main"                                 \
+        --email="s3_main@localhost.invalid"             \
+        --display-name="s3_main"                        \
+        --access-key="f05e187d8d4f43a7972b03a92681194a" \
+        --secret="345409b779a94dd3bf235f6551b76bd9"
+
+./radosgw-admin user create                             \
+        --uid="s3_alt"                                  \
+        --email="s3_alt@localhost.invalid"              \
+        --display-name="s3_alt"                         \
+        --access-key="755882dc6eff471f81c7188c124316ee" \
+        --secret="fc602931d9d54692bfc083c91868e159"
