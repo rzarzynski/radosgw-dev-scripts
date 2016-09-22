0e796de0968920de9098ed0104b8566873f859a6
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2016-09-22 14:50:30 +0200

    {init,start}_radosgw.sh: add support for CMake-based builds of Ceph.
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- init_radosgw.sh
+++ init_radosgw.sh
@@ -1,13 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-./ceph-authtool --create-keyring                \
-                ./ceph.client.radosgw.keyring
+if [ "${PWD##*/}" == "build" ]
+then
+  BINDIR="bin"
+else
+  BINDIR="."
+fi
 
-./ceph-authtool -n client.radosgw.gateway       \
-                --gen-key                       \
-                ./ceph.client.radosgw.keyring
+${BINDIR}/ceph-authtool     --create-keyring            \
+                            ./ceph.client.radosgw.keyring
 
-./ceph-authtool -n client.radosgw.gateway       \
-                --cap osd 'allow rwx'           \
-                --cap mon 'allow rwx'           \
-                ./ceph.client.radosgw.keyring
+${BINDIR}/ceph-authtool -n client.radosgw.gateway       \
+                        --gen-key                       \
+                        ./ceph.client.radosgw.keyring
+
+${BINDIR}/ceph-authtool -n client.radosgw.gateway       \
+                        --cap osd 'allow rwx'           \
+                        --cap mon 'allow rwx'           \
+                        ./ceph.client.radosgw.keyring
--- start_radosgw.sh
+++ start_radosgw.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 set -e
 
+if [ "${PWD##*/}" == "build" ]
+then
+  BINDIR="bin"
+else
+  BINDIR="."
+fi
+
 STATICS=$(dirname ${BASH_SOURCE[0]})/statics
 
-./ceph auth add client.radosgw.gateway -i ./ceph.client.radosgw.keyring
+${BINDIR}/ceph auth add client.radosgw.gateway -i ./ceph.client.radosgw.keyring
 
 if [ ! -f ceph-rgw.conf ]; then
     cp -f ${STATICS}/ceph-rgw.conf ./
 fi
 
-exec ./radosgw -n client.radosgw.gateway -c ./ceph-rgw.conf -d $@
+exec ${BINDIR}/radosgw -n client.radosgw.gateway -c ./ceph-rgw.conf \
+                       --rgw_socket_path /tmp/rgw.fcgi.sock \
+                       -d $@
 
 # Keystone v3
 # ../../radosgw-dev-scripts/start_radosgw.sh -d --debug_rgw 20 --rgw_swift_enforce_content_length true --rgw_keystone_admin_domain "Default" --rgw_keystone_api_version 3 --rgw_keystone_admin_project "admin"
