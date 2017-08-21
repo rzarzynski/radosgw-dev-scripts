3c9e569071716eaf0f78c520bb9bf7123432ca59
Radoslaw Zarzynski <rzarzyns@redhat.com>
2017-08-21 07:27:23 -0400

    create_swift_user.sh: add support for Swift's functional tests.
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- create_swift_user.sh
+++ create_swift_user.sh
@@ -8,15 +8,39 @@ else
   BINDIR="."
 fi
 
+# The first credential for both manual testing and the Functional
+# Tests of Swift
 ${BINDIR}/radosgw-admin user create                             \
                 --uid="test"                                    \
+                --tenant="test"                                 \
                 --display-name="John Doe"                       \
                 --email="john@example.com"                      &&
 ${BINDIR}/radosgw-admin subuser create                          \
                 --uid="test"                                    \
+                --tenant="test"                                 \
                 --subuser="test:tester"                         \
                 --access=full                                   &&
 ${BINDIR}/radosgw-admin key create                              \
+                --uid="test"                                    \
+                --tenant="test"                                 \
                 --subuser="test:tester"                         \
                 --key-type=swift                                \
                 --secret="testing"
+
+# The second credentuals solely for the functional tests
+${BINDIR}/radosgw-admin user create                             \
+                --uid="test2"                                   \
+                --tenant="test2"                                \
+                --display-name="John Doe 2"                     \
+                --email="john2@example.com"                     &&
+${BINDIR}/radosgw-admin subuser create                          \
+                --uid="test2"                                   \
+                --tenant="test2"                                \
+                --subuser="test2:tester2"                       \
+                --access=full                                   &&
+${BINDIR}/radosgw-admin key create                              \
+                --uid="test2"                                   \
+                --tenant="test2"                                \
+                --subuser='test2:tester2'                       \
+                --key-type=swift                                \
+                --secret="testing2"
